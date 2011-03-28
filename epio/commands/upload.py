026289fa13e09020d9d075c0d8148172d211df14
Andrew Godwin <andrew@aeracode.org>
2011-03-28 17:04:51 +0100

    Ignore .gitignore files

--- epio/commands/upload.py
+++ epio/commands/upload.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import tempfile
 from epio.commands import AppNameCommand, CommandError
 
 class Command(AppNameCommand):
@@ -14,48 +15,66 @@ class Command(AppNameCommand):
         
         print "Uploading %s as app %s" % (os.path.abspath("."), app)
         # Make a temporary git repo, commit the current directory to it, and push
-        if os.path.exists(".epio-git"):
-            subprocess.call(["rm", "-rf", ".epio-git"])
-        env = dict(os.environ)
-        env.update({"GIT_DIR": ".epio-git", "GIT_WORK_TREE": "."})
-        subprocess.Popen(
-            ["git", "init"],
-            env=env,
-            stdout=subprocess.PIPE,
-        ).communicate()
-        # Create a local ignore file
-        fh = open(".epio-git/info/exclude", "w")
-        fh.write(".git\n.hg\n.svn\n.epio-git\n")
-        if os.path.isfile(".epioignore"):
-            fh2 = open(".epioignore")
-            fh.write(fh2.read())
-            fh2.close()
-        fh.close()
-        # Set configuration options
-        fh = open(".epio-git/config", "w")
-        fh.write("[core]\nautocrlf = false\n")
-        fh.close()
-        # Add files into git
-        subprocess.Popen(
-            ["git", "add", "."],
-            env=env,
-            stdout=subprocess.PIPE,
-        ).communicate()
-        # Commit them all
-        subprocess.Popen(
-            ["git", "commit", "-a", "-m", "Auto-commit."],
-            env=env,
-            stdout=subprocess.PIPE,
-        ).communicate()
-        # Push it
-        subprocess.call(
-            ["git", "push", "vcs@%s:%s" % (
-                os.environ.get('EPIO_HOST', 'upload.ep.io').split(":")[0],
-                app,
-            ), "master"],
-            env=env,
-        )
-        # Remove the old repo
-        subprocess.call(["rm", "-rf", ".epio-git"])
+        repo_dir = tempfile.mkdtemp(prefix="epio-upload-")
+        try:
+            # Copy the files across
+            subprocess.Popen(
+                ["cp", "-RT", ".", repo_dir],
+                stdout=subprocess.PIPE,
+                cwd = os.getcwd(),
+            ).communicate()
+            # Init the git repo
+            env = dict(os.environ)
+            subprocess.Popen(
+                ["git", "init"],
+                env=env,
+                stdout=subprocess.PIPE,
+                cwd=repo_dir,
+            ).communicate()
+            # Create a local ignore file
+            fh = open(os.path.join(repo_dir, ".git/info/exclude"), "w")
+            fh.write(".git\n.hg\n.svn\n.epio-git\n")
+            if os.path.isfile(".epioignore"):
+                fh2 = open(".epioignore")
+                fh.write(fh2.read())
+                fh2.close()
+            fh.close()
+            # Remove any gitignore file
+            subprocess.Popen(
+                ["rm", "-f", ".gitignore"],
+                env=env,
+                stdout=subprocess.PIPE,
+                cwd=repo_dir,
+            ).communicate()
+            # Set configuration options
+            fh = open(os.path.join(repo_dir, ".git/config"), "w")
+            fh.write("[core]\nautocrlf = false\n")
+            fh.close()
+            # Add files into git
+            subprocess.Popen(
+                ["git", "add", "."],
+                env=env,
+                stdout=subprocess.PIPE,
+                cwd=repo_dir,
+            ).communicate()
+            # Commit them all
+            subprocess.Popen(
+                ["git", "commit", "-a", "-m", "Auto-commit."],
+                env=env,
+                stdout=subprocess.PIPE,
+                cwd=repo_dir,
+            ).communicate()
+            # Push it
+            subprocess.call(
+                ["git", "push", "vcs@%s:%s" % (
+                    os.environ.get('EPIO_HOST', 'upload.ep.io').split(":")[0],
+                    app,
+                ), "master"],
+                env=env,
+                cwd=repo_dir,
+            )
+        finally:
+            # Remove the repo
+            subprocess.call(["rm", "-rf", repo_dir])
 
 
