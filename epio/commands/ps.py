7b0970af3f0d6fc53da7b3bda25894115c7e7b47
Andrew Godwin <andrew@aeracode.org>
2011-08-08 16:00:01 +0100

    Add "epio ps" command

---
+++ epio/commands/ps.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from epio.commands import AppCommand, CommandError
+
+class Command(AppCommand):
+    help = 'Shows processes currently running for this app.'
+
+    def handle_app(self, app, **options):
+        client = self.get_client(options)
+
+        # Get the current processes
+        response, content = client.get('app/%s/processes/' % app)
+
+        if response.status == 200:
+            print "%-10s  %-20s  %s" % ("STATE", "LOCATION", "TYPE")
+            for type, instances in content['processes'].items():
+                for host, port, state in instances:
+                    if host.endswith(".ep.io"):
+                        host = host[:-6]
+                    print "%-10s  %-20s  [%s]" % (
+                        state,
+                        "%s:%s" % (host, port),
+                        type,
+                    )
+        else:
+            raise CommandError("Unknown error, %s: %s" % (response.status, content))
+
+
