8efd57e7c941056e52ac81dbbd2a7a63d340c1a8
Andrew Godwin <andrew@aeracode.org>
2011-10-27 15:16:44 +0100

    Rename "run_command" to "run"

---
+++ epio/commands/run.py
@@ -0,0 +1,15 @@
+import os
+from epio.commands import AppNameCommand
+
+class Command(AppNameCommand):
+    help = 'Run an arbitary command.'
+
+    def handle_app_name(self, app_name, *args, **options):
+        # We just use SSH.
+        os.execvp("ssh", [
+            "ssh",
+            "-t",
+            "vcs@%s" % os.environ.get('EPIO_UPLOAD_HOST', os.environ.get('EPIO_HOST', 'upload.ep.io')).split(":")[0],
+            "epio_command",
+            app_name,
+        ] + list(args))
--- epio/commands/run_command.py
+++ epio/commands/run_command.py
@@ -1,15 +1,2 @@
-import os
-from epio.commands import AppNameCommand
-
-class Command(AppNameCommand):
-    help = 'Run an arbitary command.'
-
-    def handle_app_name(self, app_name, *args, **options):
-        # We just use SSH.
-        os.execvp("ssh", [
-            "ssh",
-            "-t",
-            "vcs@%s" % os.environ.get('EPIO_UPLOAD_HOST', os.environ.get('EPIO_HOST', 'upload.ep.io')).split(":")[0],
-            "epio_command",
-            app_name,
-        ] + list(args))
+# Aliasing run_command to run
+from epio.commands.run import Command
